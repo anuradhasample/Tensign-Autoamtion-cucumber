1682d12bab21e14d98057ef4518e3134334028a2
anuradhasample <anuradha.jayena@bhavnacorp.com>
2023-12-13 15:00:42 +0530

    updated cypress config

--- cypress.config.js
+++ cypress.config.js
@@ -1,4 +1,3 @@
-
 const { defineConfig } = require("cypress");
 const cucumber = require("cypress-cucumber-preprocessor").default;
 const { configureAllureAdapterPlugins } = require("@mmisty/cypress-allure-adapter/plugins");
